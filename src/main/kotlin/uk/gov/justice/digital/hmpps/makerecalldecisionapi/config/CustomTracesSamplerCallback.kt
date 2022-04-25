fa4e0683a12180a552153fe267e448b24925f73a
Darren Oakley <daz.oakley@gmail.com>
2022-04-25 16:36:42 +0100

    MRD-10: Sentry Setup (#11)
    * MRD-10: Setup Sentry
    
    This sets up Sentry error reporting and (limited) performance
    monitoring. All error reports also include the Application Insights
    Operation ID so we can link them to a full trace.
    
    * MRD-10: Report deployments to Sentry
    
    * Fix import ordering

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/config/CustomTracesSamplerCallback.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.makerecalldecisionapi.config
+
+import io.sentry.SamplingContext
+import io.sentry.SentryOptions.TracesSamplerCallback
+import org.springframework.stereotype.Component
+import javax.servlet.http.HttpServletRequest
+
+const val defaultSampleRate = 0.05
+const val noSampleRate = 0.0
+
+@Component
+class CustomTracesSamplerCallback : TracesSamplerCallback {
+  override fun sample(context: SamplingContext): Double =
+    context.customSamplingContext?.let { customSamplingContext ->
+      val request = customSamplingContext["request"] as HttpServletRequest
+      when (request.requestURI) {
+        // The health check endpoints are just noise - drop all transactions
+        "/health/liveness" -> noSampleRate
+        "/health/readiness" -> noSampleRate
+        else -> defaultSampleRate
+      }
+    } ?: defaultSampleRate
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/config/SentryConfig.kt
@@ -0,0 +1,49 @@
+package uk.gov.justice.digital.hmpps.makerecalldecisionapi.config
+
+import io.opentelemetry.api.trace.Span
+import io.sentry.Sentry
+import io.sentry.SentryEvent
+import io.sentry.SentryOptions
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class SentryContextAppender : HandlerInterceptor {
+  private val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+  @Throws(Exception::class)
+  override fun preHandle(
+    request: HttpServletRequest,
+    response: HttpServletResponse,
+    handler: Any?
+  ): Boolean {
+    val operationId: String = Span.current().spanContext.traceId
+
+    log.info("[preHandle] ${request.method} ${request.requestURI} - operationId: $operationId")
+
+    Sentry.configureScope { scope ->
+      scope.setContexts("appInsightsOperationId", operationId)
+    }
+
+    return true
+  }
+}
+
+@Component
+class SentryBeforeSendCallback : SentryOptions.BeforeSendCallback {
+  private val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+  override fun execute(event: SentryEvent, hint: Any?): SentryEvent? {
+    log.info("event: '{}', throwable: '{}', hint: '{}'", event, event.throwable, hint)
+
+    if (event.throwable is ClientTimeoutException) {
+      return null
+    }
+
+    return event
+  }
+}
