a6505c86f21ca3e4405749f6de03216321bdea3c
Bill Sclater <bill.sclater@equalexperts.com>
2022-05-06 23:15:36 +0100

    MRD-43_MRD-120_person-details-and-overview: 1. Implemented person details endpoint 2. Implemented overview endpoint

--- src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/integration/client/CommunityApiClientTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/integration/client/CommunityApiClientTest.kt
@@ -10,7 +10,7 @@ import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.Ad
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.AddressStatus
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.AllOffenderDetailsResponse
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.ContactDetails
-import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.Conviction
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.ConvictionResponse
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.Custody
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.CustodyStatus
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.Offence
@@ -18,10 +18,14 @@ import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.Of
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.OffenderManager
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.OrderManager
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.ProviderEmployee
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.Registration
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.RegistrationsResponse
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.Sentence
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.SentenceType
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.Staff
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.Team
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.TrustOfficer
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.communityapi.Type
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.integration.IntegrationTestBase
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -39,7 +43,7 @@ class CommunityApiClientTest : IntegrationTestBase() {
     unallocatedConvictionResponse(crn, staffCode)
 
     // and
-    val expected = Conviction(
+    val expected = ConvictionResponse(
       convictionDate = LocalDate.parse("2021-06-10"),
       sentence = Sentence(
         startDate = LocalDate.parse("2022-04-26"),
@@ -55,7 +59,7 @@ class CommunityApiClientTest : IntegrationTestBase() {
           mainOffence = true,
           detail = OffenceDetail(
             mainCategoryDescription = "string", subCategoryDescription = "string",
-            description = "string"
+            description = "Robbery (other than armed robbery)"
           )
         )
       ),
@@ -90,11 +94,25 @@ class CommunityApiClientTest : IntegrationTestBase() {
       dateOfBirth = LocalDate.parse("1982-10-24"),
       firstName = "John",
       surname = "Smith",
+      gender = "Male",
       contactDetails = ContactDetails(
         addresses = listOf(
           Address(
+            postcode = "S3 7BS",
+            district = "Sheffield City Centre",
+            addressNumber = "32",
+            buildingName = "HMPPS Digital Studio",
             town = "Sheffield",
-            county = "South Yorkshire", status = AddressStatus(code = "ABC123", description = "Some description")
+            county = "South Yorkshire", status = AddressStatus(code = "ABC123", description = "Main")
+          ),
+          Address(
+            town = "Sheffield",
+            county = "South Yorkshire",
+            buildingName = "HMPPS Digital Studio",
+            district = "Sheffield City Centre",
+            status = AddressStatus(code = "ABC123", description = "Not Main"),
+            postcode = "S3 7BS",
+            addressNumber = "33"
           )
         )
       ),
@@ -103,13 +121,59 @@ class CommunityApiClientTest : IntegrationTestBase() {
           active = true,
           trustOfficer = TrustOfficer(forenames = "Sheila Linda", surname = "Hancock"),
           staff = Staff(forenames = "Sheila Linda", surname = "Hancock"),
-          providerEmployee = ProviderEmployee(forenames = "Sheila Linda", surname = "Hancock")
+          providerEmployee = ProviderEmployee(forenames = "Sheila Linda", surname = "Hancock"),
+          team = Team(
+            telephone = "09056714321",
+            emailAddress = "first.last@digital.justice.gov.uk",
+            code = "C01T04",
+            description = "OMU A"
+          )
+        ),
+        OffenderManager(
+          active = false,
+          trustOfficer = TrustOfficer(forenames = "Dua", surname = "Lipa"),
+          staff = Staff(forenames = "Sheila Linda", surname = "Hancock"),
+          providerEmployee = ProviderEmployee(forenames = "Sheila Linda", surname = "Hancock"),
+          team = Team(
+            telephone = "123",
+            emailAddress = "dua.lipa@digital.justice.gov.uk",
+            code = "C01T04",
+            description = "OMU A"
+          )
         )
+
       )
     )
 
     // when
     val actual = communityApiClient.getAllOffenderDetails(crn).block()
+
+    // then
+    assertThat(actual, equalTo(expected))
+  }
+
+  @Test
+  fun `retrieves registrations`() {
+    // given
+    val crn = "X123456"
+    registrationsResponse(crn)
+
+    // and
+    val expected = RegistrationsResponse(
+      registrations = listOf(
+        Registration(
+          active = true,
+          type = Type(code = "ABC123", description = "Victim contact")
+        ),
+        Registration(
+          active = false,
+          type = Type(code = "ABC124", description = "Mental health issues")
+        )
+      )
+    )
+
+    // when
+    val actual = communityApiClient.getRegistrations(crn).block()
 
     // then
     assertThat(actual, equalTo(expected))
