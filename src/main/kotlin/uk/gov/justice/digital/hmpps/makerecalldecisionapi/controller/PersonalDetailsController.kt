90fce23a925d38888c9acb2902d22f30463ffd22
Jon Wyatt <jonnywyatt2@gmail.com>
2022-05-09 11:03:24 +0100

    MRD-117 - Mock data for personal details page of case summary (#39)
    Co-authored-by: Jon Wyatt <>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/controller/PersonalDetailsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/controller/PersonalDetailsController.kt
@@ -13,25 +13,25 @@ import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.FullName
 
 @RestController
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-class OverviewController {
+class PersonalDetailsController {
   companion object {
     private val log = LoggerFactory.getLogger(this::class.java)
   }
 
   @PreAuthorize("hasRole('ROLE_MAKE_RECALL_DECISION')")
-  @GetMapping("/cases/{crn}/overview")
-  @Operation(summary = "WIP: Returns an overview of the case details")
-  fun overview(@PathVariable("crn") crn: Crn): String {
-    log.info("Overview endpoint hit for CRN: $crn")
-    return overviewResponse()
+  @GetMapping("/cases/{crn}/personal-details")
+  @Operation(summary = "WIP: Returns personal details")
+  fun personalDetails(@PathVariable("crn") crn: Crn): String {
+    log.info("Personal details endpoint hit for CRN: $crn")
+    return personalDetailsResponse()
   }
 }
 
-data class OverviewResponse(
+data class PersonalDetailsResponse(
   val name: FullName
 )
 
-fun overviewResponse() = """
+fun personalDetailsResponse() = """
 {
   "personDetails": {
     "name": "Paula Smith",
@@ -40,15 +40,23 @@ fun overviewResponse() = """
     "gender": "Male",
     "crn": "A12345"
   },
-  "offences": [
-    {
-      "mainOffence": true,
-      "description": "Robbery (other than armed robbery)"
-    },
-    {
-      "mainOffence": false,
-      "description": "Shoplifting"
+  "currentAddress": {
+    "line1": "5 Anderton Road",
+    "line2": "Newham",
+    "town": "London",
+    "postcode": "E15 1UJ"
+  },
+  "offenderManager": {
+    "name": "Jenny Eclair",
+    "phoneNumber": "07824637629",
+    "email": "jenny@probation.com",
+    "probationTeam": {
+      "code": "N07",
+      "label": "NPS London"
     }
-  ]
+  },
+  "risk": {
+    "flags": ["Victim contact", "Mental health issues", "MAPPA"]
+  }
 }
 """.trimIndent()
