a544eefdcb5a1f1809e6a52046ceba3ef224583b
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-05-09 16:09:42 +0100

    Add in healthchecks, but they're broken (bean issues)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/health/HmppsAuthHealth.kt
@@ -0,0 +1,12 @@
+package uk.gov.justice.digital.hmpps.makerecalldecisionapi.health
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+
+@Component("hmppsAuth")
+class HmppsAuthHealth(
+  webClientNoAuthNoMetrics: WebClient,
+  @Value("hmppsAuth") componentName: String,
+  @Value("\${oauth.endpoint.url}") hmppsAuthEndpointUrl: String
+) : PingHealthCheck(webClientNoAuthNoMetrics, componentName, "$hmppsAuthEndpointUrl/health/ping")
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/health/PingHealthCheck.kt
@@ -0,0 +1,68 @@
+package uk.gov.justice.digital.hmpps.makerecalldecisionapi.health
+
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tags
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.boot.actuate.health.Status
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.core.publisher.Mono
+import java.time.Duration
+
+abstract class PingHealthCheck(
+  private val webClient: WebClient,
+  private val componentName: String,
+  private val healthUrl: String,
+  private val timeout: Duration = Duration.ofSeconds(1)
+) : HealthIndicator {
+
+  @Autowired
+  private val meterRegistry: MeterRegistry? = null
+
+  override fun health(): Health? {
+    val result = try {
+      webClient.get()
+        .uri(healthUrl)
+        .retrieve()
+        .toEntity(String::class.java)
+        .flatMap { upWithStatus(it) }
+        .onErrorResume(WebClientResponseException::class.java) { downWithResponseBody(it) }
+        .onErrorResume(Exception::class.java) { downWithException(it) }
+        .block(timeout)
+    } catch (ex: Exception) {
+      Health.unknown().withBody(ex.toString()).build()
+    }
+
+    recordHealthMetric(result)
+
+    return result
+  }
+
+  private fun recordHealthMetric(result: Health?) {
+    var gaugeVal = 0
+
+    if (result?.status == Status.UP) {
+      gaugeVal = 1
+    }
+
+    meterRegistry?.gauge("upstream_healthcheck", Tags.of("service", componentName), gaugeVal)
+  }
+
+  private fun downWithException(it: Exception) = Mono.just(Health.down(it).build())
+
+  private fun downWithResponseBody(it: WebClientResponseException) =
+    Mono.just(
+      Health.down(it).withBody(it.responseBodyAsString).withHttpStatus(it.statusCode).build()
+    )
+
+  private fun upWithStatus(it: ResponseEntity<String>): Mono<Health> =
+    Mono.just(Health.up().withHttpStatus(it.statusCode).build())
+
+  private fun Health.Builder.withHttpStatus(status: HttpStatus) = this.withDetail("status", status)
+
+  private fun Health.Builder.withBody(body: String) = this.withDetail("body", body)
+}
