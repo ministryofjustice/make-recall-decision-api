24547744f66f3c8b03e6447608b91659f8e8d990
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-05-10 17:00:25 +0100

    Fix the gotenberg check in the start script

--- scripts/start-local-services.sh
+++ scripts/start-local-services.sh
@@ -86,7 +86,7 @@ function wait_for {
 wait_for "http://localhost:9090/auth/health/ping" "${AUTH_NAME}"
 wait_for "http://localhost:3000/ping" "${UI_NAME}"
 wait_for "http://localhost:8081/health/readiness" "${API_NAME}"
-wait_for "http://9091/health" "gotenberg"
+wait_for "http://localhost:9091/health" "gotenberg"
 
 printf "\n\nAll services started.\n\n"
 printf "\n\nLogs for API and UI can be found by running:\n"
