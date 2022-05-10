7029046f7914cfb809b215920a188e7766ae610b
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-05-10 15:15:03 +0100

    MRD-39: Add healthchecks for the other upstreams

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/health/CommunityApiHealth.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/health/CommunityApiHealth.kt
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
-@Component("hmppsAuth")
-class HmppsAuthHealth(
+@Component("communityApi")
+class CommunityApiHealth(
   webClientNoAuthNoMetrics: WebClient,
-  @Value("hmppsAuth") componentName: String,
-  @Value("\${hmpps.auth.url}") endpointUrl: String
-) : PingHealthCheck(webClientNoAuthNoMetrics, componentName, "$endpointUrl/health/ping")
+  @Value("communityApi") componentName: String,
+  @Value("\${community.endpoint.url}") endpointUrl: String
+) : PingHealthCheck(webClientNoAuthNoMetrics, componentName, "$endpointUrl/ping")
