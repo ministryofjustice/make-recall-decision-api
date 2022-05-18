7d53dc8019c29d536e86f26fda9f049c49d87cbd
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-05-18 10:55:29 +0100

    Add Z back to contactStartDate field (#81)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/ContactSummaryResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/ContactSummaryResponse.kt
@@ -1,10 +1,8 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.OffsetDateTime
 
 data class ContactSummaryResponse(
-  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", shape = JsonFormat.Shape.STRING)
   val contactStartDate: OffsetDateTime?,
   val descriptionType: String?,
   val outcome: String? = null,
