f0b0ee250e6cc15101bf2eacd83b102f13e980e5
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-05-31 09:44:52 +0100

    MRD-148 Licence history code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/ContactSummaryResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/ContactSummaryResponse.kt
@@ -5,6 +5,7 @@ import java.time.OffsetDateTime
 data class ContactSummaryResponse(
   val contactStartDate: OffsetDateTime?,
   val descriptionType: String?,
+  val code: String?,
   val outcome: String? = null,
   val notes: String? = null,
   val enforcementAction: String? = null,
