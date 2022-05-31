ec9cd3ff0b39e044a44145bef61672d27efbc47e
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-05-31 12:24:41 +0100

    MRD-184 Include inactive licence conditions in response

--- src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/integration/responses/ndelius/licenceconditions/NoActiveLicencesResponse.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/integration/responses/ndelius/licenceconditions/NoActiveLicencesResponse.kt
@@ -1,21 +1,7 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.integration.responses.ndelius.licenceconditions
 
-fun noActiveLicences(convictionId: Long) = """
+fun noActiveOrInactiveLicences() = """
 {
-     "licenceConditions": [
-     {
-          "startDate": "2022-05-18",
-          "createdDateTime": "2022-05-18T19:33:56",
-          "active": false,
-          "licenceConditionTypeMainCat": {
-              "code": "NLC8",
-              "description": "Freedom of movement for conviction $convictionId"
-          },
-          "licenceConditionTypeSubCat": {
-              "code": "NSTT8",
-              "description": "To only attend places of worship which have been previously agreed with your supervising officer."
-          }
-     }
-     ]
+     "licenceConditions": []
 }
 """.trimIndent()
