34a5e058f903f68953b13ccf6643c46e5ce85979
Bill Sclater <bill.sclater@equalexperts.com>
2022-06-03 12:59:04 +0100

    MRD-112_restrict-permissions-for-sensitive-cases: Updated clients to use user credentials to access NDellius.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/PersonDetailsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/PersonDetailsService.kt
@@ -1,7 +1,8 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.service
 
-import kotlinx.coroutines.reactive.awaitFirst
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.client.CommunityApiClient
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.CurrentAddress
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.OffenderManager
@@ -9,11 +10,14 @@ import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecis
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.PersonDetailsResponse
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.ProbationTeam
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.ndelius.AllOffenderDetailsResponse
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.exception.ClientTimeoutException
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.exception.PersonNotFoundException
 import java.time.LocalDate
 
 @Service
 class PersonDetailsService(
-  private val communityApiClient: CommunityApiClient
+  @Qualifier("communityApiClientUserEnhanced") private val communityApiClient: CommunityApiClient,
+//  private val communityApiClient: CommunityApiClient
 ) {
   suspend fun getPersonDetails(crn: String): PersonDetailsResponse {
     val offenderDetails = getPersonalDetailsOverview(crn)
@@ -73,8 +77,21 @@ class PersonDetailsService(
   }
 
   private suspend fun getPersonalDetailsOverview(crn: String): AllOffenderDetailsResponse {
-    return communityApiClient.getAllOffenderDetails(crn).awaitFirst()
+    return getValue(communityApiClient.getAllOffenderDetails(crn))!!
   }
 
   private fun age(offenderDetails: AllOffenderDetailsResponse) = offenderDetails.dateOfBirth?.until(LocalDate.now())?.years
+
+  private fun getValue(mono: Mono<AllOffenderDetailsResponse>): AllOffenderDetailsResponse {
+    return try {
+      val allOffenderDetailsResponse = mono.block()
+      allOffenderDetailsResponse ?: allOffenderDetailsResponse
+    } catch (wrappedException: RuntimeException) {
+      when (wrappedException.cause) {
+        is ClientTimeoutException -> throw wrappedException.cause as ClientTimeoutException
+        is PersonNotFoundException -> throw wrappedException.cause as PersonNotFoundException
+        else -> throw wrappedException
+      }
+    }
+  }
 }
