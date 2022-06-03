88fed5bd0170c69747866ccdc1d8987638caee65
Bill Sclater <bill.sclater@equalexperts.com>
2022-06-03 12:59:04 +0100

    MRD-112_restrict-permissions-for-sensitive-cases: Updated clients to use user credentials to access NDellius.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/CaseSummaryOverviewService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/CaseSummaryOverviewService.kt
@@ -1,25 +1,29 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.service
 
-import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitFirstOrNull
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.client.CommunityApiClient
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.CaseSummaryOverviewResponse
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.Offence
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.PersonDetails
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.Risk
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.exception.ClientTimeoutException
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.exception.NoActiveConvictionsException
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.exception.PersonNotFoundException
 import java.time.LocalDate
 
 @Service
 class CaseSummaryOverviewService(
-  private val communityApiClient: CommunityApiClient
+  @Qualifier("communityApiClientUserEnhanced") private val communityApiClient: CommunityApiClient
 ) {
   suspend fun getOverview(crn: String): CaseSummaryOverviewResponse {
-    val offenderDetails = communityApiClient.getAllOffenderDetails(crn).awaitFirst()
-    val activeConvictions = communityApiClient.getActiveConvictions(crn).awaitFirst()
-    val age = offenderDetails?.dateOfBirth?.until(LocalDate.now())?.years
-    val firstName = offenderDetails?.firstName ?: ""
-    val surname = offenderDetails?.surname ?: ""
+    val offenderDetails = getValue(communityApiClient.getAllOffenderDetails(crn))!!
+    val activeConvictions = getValue(communityApiClient.getActiveConvictions(crn)) ?: emptyList()
+    val age = offenderDetails.dateOfBirth?.until(LocalDate.now())?.years
+    val firstName = offenderDetails.firstName ?: ""
+    val surname = offenderDetails.surname ?: ""
     val name = if (firstName.isEmpty()) {
       surname
     } else "$firstName $surname"
@@ -39,13 +43,27 @@ class CaseSummaryOverviewService(
     return CaseSummaryOverviewResponse(
       personalDetailsOverview = PersonDetails(
         name = name,
-        dateOfBirth = offenderDetails?.dateOfBirth,
+        dateOfBirth = offenderDetails.dateOfBirth,
         age = age,
-        gender = offenderDetails?.gender ?: "",
+        gender = offenderDetails.gender ?: "",
         crn = crn
       ),
       offences = offences.filter { it.mainOffence == true },
       risk = Risk(flags = riskFlags)
     )
+  }
+
+  private fun <T : Any> getValue(mono: Mono<T>?): T? {
+    return try {
+      val value = mono?.block()
+      value ?: value
+    } catch (wrappedException: RuntimeException) {
+      when (wrappedException.cause) {
+        is ClientTimeoutException -> throw wrappedException.cause as ClientTimeoutException
+        is PersonNotFoundException -> throw wrappedException.cause as PersonNotFoundException
+        is NoActiveConvictionsException -> throw wrappedException.cause as NoActiveConvictionsException
+        else -> throw wrappedException
+      }
+    }
   }
 }
