66205dc1a5b9f8ee104314fb01a5e9d61e6ee79f
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-06-07 16:56:10 +0100

    Mrd 112 restrict permissions for sensitive cases v5 (#144)
    * MRD-112_restrict-permissions-for-sensitive-cases: Updated clients to use user credentials to access NDellius.
    
    * empty commit
    
    * Remove blank lines
    
    Co-authored-by: Bill Sclater <bill.sclater@equalexperts.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/LicenceHistoryService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/LicenceHistoryService.kt
@@ -1,16 +1,20 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.service
 
-import kotlinx.coroutines.reactive.awaitFirstOrNull
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.client.CommunityApiClient
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.ContactSummaryResponse
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.LicenceHistoryResponse
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.ndelius.ReleaseSummaryResponse
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.exception.ClientTimeoutException
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.exception.NoActiveConvictionsException
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.exception.ReleaseDetailsNotFoundException
 import kotlin.streams.toList
 
 @Service
 class LicenceHistoryService(
-  private val communityApiClient: CommunityApiClient,
+  @Qualifier("communityApiClientUserEnhanced") private val communityApiClient: CommunityApiClient,
   private val personDetailsService: PersonDetailsService
 ) {
 
@@ -27,9 +31,9 @@ class LicenceHistoryService(
   }
 
   private suspend fun getContactSummary(crn: String, filterContacts: Boolean): List<ContactSummaryResponse> {
-    val contactSummaryResponse = communityApiClient.getContactSummary(crn, filterContacts).awaitFirstOrNull()?.content
+    val contactSummaryResponse = getValue(communityApiClient.getContactSummary(crn, filterContacts))
 
-    return contactSummaryResponse
+    return contactSummaryResponse?.content
       ?.stream()
       ?.map {
         ContactSummaryResponse(
@@ -45,6 +49,19 @@ class LicenceHistoryService(
   }
 
   private suspend fun getReleaseSummary(crn: String): ReleaseSummaryResponse? {
-    return communityApiClient.getReleaseSummary(crn).awaitFirstOrNull()
+    return getValue(communityApiClient.getReleaseSummary(crn))
+  }
+
+  private fun <T : Any> getValue(mono: Mono<T>?): T? {
+    return try {
+      val value = mono?.block()
+      value ?: value
+    } catch (wrappedException: RuntimeException) {
+      when (wrappedException.cause) {
+        is ClientTimeoutException -> throw wrappedException.cause as ClientTimeoutException
+        is ReleaseDetailsNotFoundException -> throw wrappedException.cause as NoActiveConvictionsException
+        else -> throw wrappedException
+      }
+    }
   }
 }
