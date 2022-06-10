02bb6e6ead8642c4d9c6cfa03063cea5f7ca8fc0
Bill Sclater <bill.sclater@equalexperts.com>
2022-06-10 14:25:51 +0100

    done

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/controller/PersonDetailsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/controller/PersonDetailsController.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.makerecalldecisionapi.controller
 import io.swagger.v3.oas.annotations.Operation
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,7 +19,6 @@ class PersonDetailsController(
     private val log = LoggerFactory.getLogger(this::class.java)
   }
 
-  // TODO make suspendable and try with await(..) run tests first, then app, then other EPS
 //  @PreAuthorize("hasRole('ROLE_MAKE_RECALL_DECISION')")
   @GetMapping("/cases/{crn}/personal-details")
   @Operation(summary = "WIP: Returns an overview of the person details")
