a90b3bcef56187fb7f0969905065d5c1fac1a3bf
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-06-14 09:38:52 +0100

    Switch csv reader to read from input stream (#162)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/reader/ContactGroupsCsvReader.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/reader/ContactGroupsCsvReader.kt
@@ -6,13 +6,11 @@ import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.csv.ContactGroup
-import java.io.File
 
 @Component
 class ContactGroupsCsvReader {
   companion object {
     private val resource: Resource = ClassPathResource("contact-groups.csv")
-    private val file: File = resource.file
     private var contactGroups: List<ContactGroup>
 
     private val log = LoggerFactory.getLogger(this::class.java)
@@ -23,7 +21,7 @@ class ContactGroupsCsvReader {
 
     init {
       log.info("Building contact groups list on application startup")
-      contactGroups = csvReader().open(file) {
+      contactGroups = csvReader().open(resource.inputStream) {
         readAllAsSequence().drop(1).map {
           ContactGroup(it)
         }.iterator().asSequence().toList()
