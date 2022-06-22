87e7a936ba0267b94bd9d6ec2514621c9c68f8ca
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-06-22 16:08:11 +0100

    MRD-268 WIP unwrap user access response

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/LicenceConditionsResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/LicenceConditionsResponse.kt
@@ -1,10 +1,11 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.ndelius.ReleaseSummaryResponse
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.ndelius.UserAccessResponse
 
 data class LicenceConditionsResponse(
-  val userAccessResponse: UserAccessResponse? = null,
+  @field:JsonUnwrapped val userAccessResponse: UserAccessResponse? = null,
   val personalDetailsOverview: PersonDetails? = null,
   val convictions: List<ConvictionResponse>? = null,
   val releaseSummary: ReleaseSummaryResponse? = null,
