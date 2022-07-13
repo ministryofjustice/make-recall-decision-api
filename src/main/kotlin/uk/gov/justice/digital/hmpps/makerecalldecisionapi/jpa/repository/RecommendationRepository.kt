98d6afa3a1f38597b682a4885dd32027cc8fde9d
Bill Sclater <bill.sclater@equalexperts.com>
2022-07-13 20:34:33 +0100

    MRD-330_save-a-recommendation-to-db:modified recommendation endpoint to create a blank recommendation and return its id

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/jpa/repository/RecommendationRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/jpa/repository/RecommendationRepository.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.repository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.entity.RecommendationEntity
-import java.util.UUID
 
 @Repository
-interface RecommendationRepository : CrudRepository<RecommendationEntity, UUID>
+interface RecommendationRepository : CrudRepository<RecommendationEntity, Long>
