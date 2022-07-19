29a565e2115a41a2eb2fd2c492d4b94dad4274b5
Bill Sclater <bill.sclater@equalexperts.com>
2022-07-19 01:39:46 +0100

    MRD-339_update-recommendation: Added update endpoint for recommendations and an activeRecommendation object on the create recommendation endpoint.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/RecommendationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/RecommendationService.kt
@@ -1,23 +1,38 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.service
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.ActiveRecommendation
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.CreateRecommendationRequest
-import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.CreateRecommendationResponse
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.RecallType
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.RecommendationResponse
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.UpdateRecommendationRequest
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.exception.NoRecommendationFoundException
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.entity.RecallTypeOption
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.entity.Recommendation
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.entity.RecommendationEntity
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.entity.RecommendationModel
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.entity.Status
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.repository.RecommendationRepository
+import java.time.LocalDateTime
 import kotlin.jvm.optionals.getOrNull
 
 @Service
 internal class RecommendationService(
   val recommendationRepository: RecommendationRepository
 ) {
-  fun createRecommendation(recommendationRequest: CreateRecommendationRequest): CreateRecommendationResponse {
-    val savedRecommendation = recommendationRepository.save(RecommendationEntity(data = RecommendationModel(crn = recommendationRequest.crn)))
-    return CreateRecommendationResponse(
-      id = savedRecommendation.id
+  fun createRecommendation(recommendationRequest: CreateRecommendationRequest, username: String?): RecommendationResponse {
+    val lastModifiedDate = LocalDateTime.now().toString()
+    val savedRecommendation = saveRecommendationEntity(recommendationRequest, username, lastModifiedDate)
+
+    return RecommendationResponse(
+      id = savedRecommendation?.id,
+      status = savedRecommendation?.data?.status,
+      activeRecommendation = ActiveRecommendation(
+        recommendationId = savedRecommendation?.id,
+        lastModifiedBy = username,
+        lastModifiedDate = lastModifiedDate
+      )
     )
   }
 
@@ -28,6 +43,87 @@ internal class RecommendationService(
     return RecommendationResponse(
       id = recommendationEntity.id,
       crn = recommendationEntity.data.crn
+    )
+  }
+
+  @OptIn(ExperimentalStdlibApi::class)
+  @Transactional
+  fun updateRecommendation(updateRecommendationRequest: UpdateRecommendationRequest, recommendationId: Long): RecommendationResponse {
+    val existingRecommendationEntity = recommendationRepository.findById(recommendationId).getOrNull()
+      ?: throw NoRecommendationFoundException("No recommendation found for id: $recommendationId")
+
+    val updatedRecommendationEntity = updateRecommendation(existingRecommendationEntity, updateRecommendationRequest)
+
+    return RecommendationResponse(
+      id = updatedRecommendationEntity!!.id!!,
+      crn = updatedRecommendationEntity.data.crn,
+      status = updatedRecommendationEntity.data.status,
+      recallType = updateRecommendationRequest.recallType
+        ?: RecallType(
+          value = updatedRecommendationEntity.data.recommendation ?: existingRecommendationEntity.data.recommendation,
+          options = useExistingOptions()
+        ),
+      activeRecommendation = null // TODO add once MRD-342 complete
+    )
+  }
+
+  private fun updateRecommendation(
+    existingRecommendationEntity: RecommendationEntity,
+    updateRecommendationRequest: UpdateRecommendationRequest
+  ): RecommendationEntity? {
+    val updatedRecallType = updateRecommendationRequest.recallType?.value
+    val originalRecallType = existingRecommendationEntity.data.recommendation?.name
+    val recallType = updatedRecallType ?: originalRecallType
+    val recommendation = if (recallType != null) {
+      Recommendation.valueOf(recallType.toString())
+    } else {
+      null
+    }
+
+    val updatedRecommendationEntity = recommendationRepository.save(
+      existingRecommendationEntity.copy(
+        id = existingRecommendationEntity.id,
+        data = RecommendationModel(
+          crn = existingRecommendationEntity.data.crn,
+          recommendation = recommendation,
+          status = existingRecommendationEntity.data.status,
+          lastModifiedDate = existingRecommendationEntity.data.lastModifiedDate,
+          lastModifiedBy = existingRecommendationEntity.data.lastModifiedBy
+        )
+      )
+    )
+    return updatedRecommendationEntity
+  }
+
+  private fun useExistingOptions() = listOf(
+    RecallTypeOption(
+      value = Recommendation.NO_RECALL.name,
+      text = Recommendation.NO_RECALL.text
+    ),
+    RecallTypeOption(
+      value = Recommendation.FIXED_TERM.name,
+      text = Recommendation.FIXED_TERM.text
+    ),
+    RecallTypeOption(
+      value = Recommendation.STANDARD.name,
+      text = Recommendation.STANDARD.text
+    )
+  )
+
+  private fun saveRecommendationEntity(
+    recommendationRequest: CreateRecommendationRequest,
+    username: String?,
+    lastModifiedDate: String?
+  ): RecommendationEntity? {
+    return recommendationRepository.save(
+      RecommendationEntity(
+        data = RecommendationModel(
+          crn = recommendationRequest.crn,
+          status = Status.DRAFT,
+          lastModifiedBy = username,
+          lastModifiedDate = lastModifiedDate
+        )
+      )
     )
   }
 }
