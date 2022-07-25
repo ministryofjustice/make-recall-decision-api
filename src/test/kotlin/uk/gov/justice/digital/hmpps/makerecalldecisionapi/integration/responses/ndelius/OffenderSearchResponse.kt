5fa5204a0a21625a6a3e29f7a482122f93916465
Bill Sclater <bill.sclater@equalexperts.com>
2022-07-25 18:38:43 +0100

    MRD-384_update-offender-search-endpoint-response-to-get-crn-from-delius: Corrected response of offender search service to use CRN given by Delius Offender Search API instead of passing back the CRN provided by the user.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/integration/responses/ndelius/OffenderSearchResponse.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/integration/responses/ndelius/OffenderSearchResponse.kt
@@ -1,12 +1,13 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.integration.responses.ndelius
 
-fun offenderSearchDeliusResponse() = """
+fun offenderSearchDeliusResponse(crn: String?) = """
 {
   "content": [
     {
       "firstName": "Pontius",
       "surname": "Pilate",
-      "dateOfBirth": "2000-11-09"
+      "dateOfBirth": "2000-11-09",
+      "otherIds": { "crn": "$crn"}
     }
   ]
 }
