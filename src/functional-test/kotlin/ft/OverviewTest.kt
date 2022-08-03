9440d29c0ce2d6dcd4669616bdefb911aa435a6e
bills7281 <104494371+bills7281@users.noreply.github.com>
2022-08-03 10:12:13 +0100

    MRD-388_MRD-389_contact-history-overview-functional-tests-enriched: added more fine grained asserts for overview and contact-history functional tests. (#264)
    Co-authored-by: Bill Sclater <bill.sclater@equalexperts.com>

--- src/functional-test/kotlin/ft/OverviewTest.kt
+++ src/functional-test/kotlin/ft/OverviewTest.kt
@@ -10,6 +10,7 @@ class OverviewTest() : FunctionalTest() {
   fun `overview, expected 200`() {
     // given
     val expected = HttpStatus.OK.value()
+    val testCrn = "X263655"
 
     // when
     lastResponse = RestAssured
@@ -19,6 +20,67 @@ class OverviewTest() : FunctionalTest() {
       .get("http://127.0.0.1:8080/cases/{crn}/overview")
 
     // then
-    assertThat(lastResponse.getStatusCode()).isEqualTo(expected)
+    assertThat(lastResponse.statusCode).isEqualTo(expected)
+    assertResponse(lastResponse, overviewExpectation())
   }
 }
+
+fun overviewExpectation() = """//TODO correct
+{
+    "userAccessResponse": null,
+    "personalDetailsOverview": {
+        "name": "Lee JarIce",
+        "dateOfBirth": "1969-06-23",
+        "age": 53,
+        "gender": "Female",
+        "crn": "X263655"
+    },
+    "convictions": [
+        {
+            "active": true,
+            "offences": [
+                {
+                    "mainOffence": true,
+                    "description": "Accident offences - 80500",
+                    "code": "80500"
+                }
+            ],
+            "sentenceDescription": "ORA Community Order",
+            "sentenceOriginalLength": 18,
+            "sentenceOriginalLengthUnits": "Months",
+            "sentenceExpiryDate": null,
+            "licenceExpiryDate": null,
+            "isCustodial": false
+        },
+        {
+            "active": true,
+            "offences": [
+                {
+                    "mainOffence": true,
+                    "description": "Accident offences - 80500",
+                    "code": "80500"
+                }
+            ],
+            "sentenceDescription": "CJA - Std Determinate Custody",
+            "sentenceOriginalLength": 12,
+            "sentenceOriginalLengthUnits": "Months",
+            "sentenceExpiryDate": null,
+            "licenceExpiryDate": null,
+            "isCustodial": true
+        }
+    ],
+    "releaseSummary": {
+        "lastRelease": null,
+        "lastRecall": null
+    },
+    "risk": {
+        "flags": [
+            "Suicide/Self Harm",
+            "Hate Crime",
+            "Street Gangs",
+            "MAPPA"
+        ]
+    },
+    "activeRecommendation": null
+}
+""".trimIndent()
