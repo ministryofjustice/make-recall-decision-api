3bf96839926c05c535dafe672252a99c557277f1
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-08-09 16:48:21 +0100

    MRD-472 Recall type field for Part A

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/recommendation/PartAData.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/recommendation/PartAData.kt
@@ -1,5 +1,11 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.recommendation
 
 data class PartAData(
-  val custodyStatus: String
+  val custodyStatus: String,
+  val recallType: RecallTypePartA,
+)
+
+data class RecallTypePartA(
+  val value: String?,
+  val details: String?
 )
