ea359ecde4b5638469e0d7a94d0571fc69dd952f
Jon Wyatt <>
2022-09-06 17:46:31 +0100

    Default custody status value to empty string

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/util/RecommendationToPartADataMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/util/RecommendationToPartADataMapper.kt
@@ -16,7 +16,7 @@ class RecommendationToPartADataMapper {
   companion object Mapper {
     fun mapRecommendationDataToPartAData(recommendation: RecommendationEntity): PartAData {
       return PartAData(
-        custodyStatus = ValueWithDetails(recommendation.data.custodyStatus?.selected?.partADisplayValue, recommendation.data.custodyStatus?.details),
+        custodyStatus = ValueWithDetails(recommendation.data.custodyStatus?.selected?.partADisplayValue ?: EMPTY_STRING, recommendation.data.custodyStatus?.details),
         recallType = findRecallTypeToDisplay(recommendation.data.recallType),
         responseToProbation = recommendation.data.responseToProbation,
         whatLedToRecall = recommendation.data.whatLedToRecall,
