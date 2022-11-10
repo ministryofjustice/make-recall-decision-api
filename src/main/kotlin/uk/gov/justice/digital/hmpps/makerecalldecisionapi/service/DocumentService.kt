7819c4f4c27134300c617eb70459a21b4b74aa4f
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-11-10 12:09:49 +0000

    MRD-870 Improve logging around licence condition service (#465)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/DocumentService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/DocumentService.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.service
 
+import org.apache.commons.lang3.StringUtils
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.core.io.Resource
 import org.springframework.http.ResponseEntity
@@ -11,9 +13,14 @@ import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.ndelius.CaseDoc
 internal class DocumentService(
   @Qualifier("communityApiClientUserEnhanced") private val communityApiClient: CommunityApiClient
 ) {
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
 
   fun getDocumentsByDocumentType(crn: String, documentType: String): List<CaseDocument>? {
+    log.info(StringUtils.normalizeSpace("About to build documents by document type $documentType for $crn in document service"))
     val groupedDocuments = getValueAndHandleWrappedException(communityApiClient.getGroupedDocuments(crn))
+    log.info(StringUtils.normalizeSpace("Got documents for $crn in document service"))
     val docs: List<CaseDocument>? = groupedDocuments?.documents?.filter { it.type?.code == documentType }
     val convictionDocs: List<CaseDocument>? = groupedDocuments?.convictions?.flatMap { e ->
       if (e.documents != null) {
@@ -24,6 +31,7 @@ internal class DocumentService(
         emptyList()
       }
     }
+    log.info(StringUtils.normalizeSpace("Built documents for $crn in document service"))
 
     return docs?.let { docList ->
       convictionDocs?.let { convictionDocsList -> docList + convictionDocsList }
