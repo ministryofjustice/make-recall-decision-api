6df4d36908e29da3aa4c70cf1a882e4047dd5bab
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-11-29 09:18:17 +0000

    MRD-900 Json ignore unknown feature flags (#495)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/featureflags/FeatureFlags.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/featureflags/FeatureFlags.kt
@@ -1,5 +1,8 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.featureflags
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class FeatureFlags(
   val flagRecommendationOffenceDetails: Boolean
 )
