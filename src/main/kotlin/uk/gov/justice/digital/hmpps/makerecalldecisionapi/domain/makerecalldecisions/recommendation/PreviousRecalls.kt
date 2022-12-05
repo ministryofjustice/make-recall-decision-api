f978ed2ae71fa2cc106ac7c5b7dc9ee914b85ddf
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-12-05 15:54:05 +0000

    MRD-898 and MRD-899 change data format (#503)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/recommendation/PreviousRecalls.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/recommendation/PreviousRecalls.kt
@@ -1,9 +1,10 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.recommendation
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 
 data class PreviousRecalls(
-  val lastRecallDate: LocalDate? = null,
+  @JsonFormat(pattern = "yyyy-MM-dd") val lastRecallDate: LocalDate? = null,
   val hasBeenRecalledPreviously: Boolean? = null,
-  val previousRecallDates: List<LocalDate>? = null
+  @JsonFormat(pattern = "yyyy-MM-dd") val previousRecallDates: List<LocalDate>? = null
 )
