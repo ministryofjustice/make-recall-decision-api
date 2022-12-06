f21e45a8daed8ae841b4b06351d77cbab224a0ab
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-12-06 09:23:18 +0000

    MRD-898 Add test for json merge fix

--- src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/integration/requests/makerecalldecisions/SecondRecommendationRequest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/integration/requests/makerecalldecisions/SecondRecommendationRequest.kt
@@ -4,6 +4,17 @@ import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.entity.Status
 
 fun secondUpdateRecommendationRequest(status: Status = Status.DRAFT) = """
 {
-  "status": "$status"
+  "status": "$status",
+  "previousReleases": {
+    "previousReleaseDates": [
+      "2015-04-24"
+    ]
+  },
+  "previousRecalls": {
+    "previousRecallDates": [
+      "2018-10-10",
+      "2016-04-30"
+    ]
+  }
 }
 """.trimIndent()
