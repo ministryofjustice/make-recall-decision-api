fcd777eaaa042a765f50275418e182d5c94b3ae3
Bill Sclater <bill.sclater@digital.justice.gov.uk>
2023-01-17 16:12:07 +0000

    commented out tests for speed

--- src/functional-test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/DocumentFunctionalTest.kt
+++ src/functional-test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/DocumentFunctionalTest.kt
@@ -1,24 +1,24 @@
-package uk.gov.justice.digital.hmpps.makerecalldecisionapi
-
-import io.restassured.RestAssured
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-
-class DocumentFunctionalTest() : FunctionalTest() {
-
-  @Test
-  fun `fetch document`() {
-
-    // when
-    lastResponse = RestAssured
-      .given()
-      .pathParam("crn", testCrn)
-      .header("Authorization", token)
-      .get("$path/cases/{crn}/documents/8821dae0-07a5-464e-8be3-e26207fa0279")
-
-    // then
-    assertThat(lastResponse.statusCode).isEqualTo(expectedOk)
-    assertThat(lastResponse.headers.get("Content-Disposition").value).isEqualTo("attachment; filename*=UTF-8''ikenberry-camploongo-d006296-66.pdf")
-    assertThat(lastResponse.headers.get("Content-Type").value).isEqualTo("application/pdf;charset=UTF-8")
-  }
-}
+//package uk.gov.justice.digital.hmpps.makerecalldecisionapi
+//
+//import io.restassured.RestAssured
+//import org.assertj.core.api.Assertions.assertThat
+//import org.junit.jupiter.api.Test
+//
+//class DocumentFunctionalTest() : FunctionalTest() {
+//
+//  @Test
+//  fun `fetch document`() {
+//
+//    // when
+//    lastResponse = RestAssured
+//      .given()
+//      .pathParam("crn", testCrn)
+//      .header("Authorization", token)
+//      .get("$path/cases/{crn}/documents/8821dae0-07a5-464e-8be3-e26207fa0279")
+//
+//    // then
+//    assertThat(lastResponse.statusCode).isEqualTo(expectedOk)
+//    assertThat(lastResponse.headers.get("Content-Disposition").value).isEqualTo("attachment; filename*=UTF-8''ikenberry-camploongo-d006296-66.pdf")
+//    assertThat(lastResponse.headers.get("Content-Type").value).isEqualTo("application/pdf;charset=UTF-8")
+//  }
+//}
--- src/functional-test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/PersonalDetailsTest.kt
+++ src/functional-test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/PersonalDetailsTest.kt
@@ -1,64 +1,64 @@
-package uk.gov.justice.digital.hmpps.makerecalldecisionapi
-
-import io.restassured.RestAssured
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-
-class PersonalDetailsTest() : FunctionalTest() {
-
-  @Test
-  fun `fetch personal details`() {
-
-    // when
-    lastResponse = RestAssured
-      .given()
-      .pathParam("crn", testCrn)
-      .header("Authorization", token)
-      .get("$path/cases/{crn}/personal-details")
-
-    // then
-    assertThat(lastResponse.statusCode).isEqualTo(expectedOk)
-    assertResponse(lastResponse, personalDetailsExpectation())
-  }
-}
-
-fun personalDetailsExpectation() = """
-{
-    "userAccessResponse": null,
-    "personalDetailsOverview": {
-        "name": "Ikenberry Camploongo",
-        "firstName": "Ikenberry",
-        "middleNames": "ZZ",
-        "surname": "Camploongo",
-        "dateOfBirth": "1986-05-11",
-        "age": 36,
-        "gender": "Male",
-        "crn": "D006296",
-        "ethnicity": "",
-        "croNumber": "",
-        "mostRecentPrisonerNumber": "",
-        "pncNumber": "",
-        "nomsNumber": ""
-    },
-    "addresses": [
-        {
-            "line1": "99 Oxford Road",
-            "line2": "",
-            "town": "Epsom",
-            "postcode": "SW16 1AF",
-            "noFixedAbode": false
-        }
-    ],
-    "offenderManager": {
-        "name": "HMPPS Auth",
-        "phoneNumber": "01111111111",
-        "email": "",
-        "probationTeam": {
-            "code": "N07CHT",
-            "label": "Automation SPG",
-            "localDeliveryUnitDescription": "All NPS London"
-        },
-        "probationAreaDescription": "NPS London"
-    }
-}
-""".trimIndent()
+//package uk.gov.justice.digital.hmpps.makerecalldecisionapi
+//
+//import io.restassured.RestAssured
+//import org.assertj.core.api.Assertions.assertThat
+//import org.junit.jupiter.api.Test
+//
+//class PersonalDetailsTest() : FunctionalTest() {
+//
+//  @Test
+//  fun `fetch personal details`() {
+//
+//    // when
+//    lastResponse = RestAssured
+//      .given()
+//      .pathParam("crn", testCrn)
+//      .header("Authorization", token)
+//      .get("$path/cases/{crn}/personal-details")
+//
+//    // then
+//    assertThat(lastResponse.statusCode).isEqualTo(expectedOk)
+//    assertResponse(lastResponse, personalDetailsExpectation())
+//  }
+//}
+//
+//fun personalDetailsExpectation() = """
+//{
+//    "userAccessResponse": null,
+//    "personalDetailsOverview": {
+//        "name": "Ikenberry Camploongo",
+//        "firstName": "Ikenberry",
+//        "middleNames": "ZZ",
+//        "surname": "Camploongo",
+//        "dateOfBirth": "1986-05-11",
+//        "age": 36,
+//        "gender": "Male",
+//        "crn": "D006296",
+//        "ethnicity": "",
+//        "croNumber": "",
+//        "mostRecentPrisonerNumber": "",
+//        "pncNumber": "",
+//        "nomsNumber": ""
+//    },
+//    "addresses": [
+//        {
+//            "line1": "99 Oxford Road",
+//            "line2": "",
+//            "town": "Epsom",
+//            "postcode": "SW16 1AF",
+//            "noFixedAbode": false
+//        }
+//    ],
+//    "offenderManager": {
+//        "name": "HMPPS Auth",
+//        "phoneNumber": "01111111111",
+//        "email": "",
+//        "probationTeam": {
+//            "code": "N07CHT",
+//            "label": "Automation SPG",
+//            "localDeliveryUnitDescription": "All NPS London"
+//        },
+//        "probationAreaDescription": "NPS London"
+//    }
+//}
+//""".trimIndent()
