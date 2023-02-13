00afdb9630cb1a2baf82f996c1ee85c55716d5c9
Bill Sclater <bill.sclater@justice.gov.uk>
2023-02-13 14:47:36 +0000

    ITs fixed

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/config/ResourceServerConfiguration.kt
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.reactive.CorsConfigurationSource
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -27,24 +24,11 @@ class ResourceServerConfiguration {
       .requestMatchers("/webjars/**").permitAll()
       .requestMatchers("/favicon.ico").permitAll()
       .requestMatchers("/csrf").permitAll()
-      .requestMatchers("/recommendations/**").hasAnyRole("MAKE_RECALL_DECISION")
       .anyRequest().authenticated()
       .and()
       .csrf()
       .disable()
       .oauth2ResourceServer().jwt().jwtAuthenticationConverter { AuthAwareTokenConverter().convert(it) }
     return http.build()
-  }
-
-  @Bean
-  fun corsConfigurationSource(): CorsConfigurationSource? {
-    val configuration = CorsConfiguration()
-    configuration.allowedOrigins = listOf("http://localhost:3000")
-    configuration.addAllowedHeader("*")
-    configuration.addAllowedMethod("*")
-    configuration.allowCredentials = true
-    val source = UrlBasedCorsConfigurationSource()
-    source.registerCorsConfiguration("/**", configuration)
-    return source
   }
 }
