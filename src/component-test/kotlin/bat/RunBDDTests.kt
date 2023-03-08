e206af0ded00ea3e125513403c0e5daed3e0fb45
Bill Sclater <bill.sclater@digital.justice.gov.uk>
2023-03-08 09:58:55 +0000

    added tags to cucumber test

--- src/component-test/kotlin/bat/RunBDDTests.kt
+++ src/component-test/kotlin/bat/RunBDDTests.kt
@@ -6,20 +6,13 @@ import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
 @CucumberOptions(
-    features = ["classpath:features/"],
+  features = ["classpath:features/"],
   glue = ["bat"],
-    plugin = [
+//  tags = "@SearchByCrn",
+  plugin = [
     "pretty",
     "json:build/cucumber-reports/json/cucumber.json",
     "html:build/cucumber-reports/html/cucumber.html"
-    ]
+  ]
 )
-
-//(
-////this code will only look into "features/" folder for features
-//features={"classpath:features/"},
-//glue = { "com.mycompany.cucumber.stepdefinitions", "com.mycompany.cucumber.hooks" },
-//format = { "com.mycompany.cucumber.formatter.RuntimeInfoCatcher", "json:target/cucumber.json" },
-//tags = { "@working" }
-//)
 class RunBDDTests
--- src/component-test/resources/features/pop-search.feature
+++ src/component-test/resources/features/pop-search.feature
@@ -1,6 +1,10 @@
 Feature: Person on probation search
 
+  @SearchByCrn
   Scenario: Searching by crn
+    Given I get a token
+
+  Scenario: Some other scenario
     Given I get a token
 
 
