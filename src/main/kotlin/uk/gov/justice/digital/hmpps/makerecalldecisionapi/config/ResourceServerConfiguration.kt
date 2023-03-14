c6f78aace50e25b5d53f32ff140865a4342ff8a3
Bill Sclater <bill.sclater@justice.gov.uk>
2023-03-14 13:20:56 +0000

    Revert "Revert "no_jira_spring-boot-ver-bump: Update plugin uk.gov.justice.hmpps.gradle-spring-boot to v5.01 (#585)" (#597)"
    This reverts commit 60e0b405083fecc54a22d36afcf1cf090576ad02.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/config/ResourceServerConfiguration.kt
@@ -1,42 +1,34 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.web.DefaultSecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
-class ResourceServerConfiguration : WebSecurityConfigurerAdapter() {
-
-  override fun configure(http: HttpSecurity) {
-    http {
-      csrf { disable() }
-      sessionManagement { sessionCreationPolicy = SessionCreationPolicy.STATELESS }
-      authorizeRequests {
-        authorize("/health/**", permitAll)
-        authorize("/info", permitAll)
-        authorize("/prometheus", permitAll)
-        authorize("/v3/api-docs/**", permitAll)
-        authorize("/swagger-ui/**", permitAll)
-        authorize("/swagger-ui.html", permitAll)
-        authorize("/swagger-resources", permitAll)
-        authorize("/swagger-resources/configuration/ui", permitAll)
-        authorize("/swagger-resources/configuration/security", permitAll)
-        authorize("/webjars/**", permitAll)
-        authorize("/favicon.ico", permitAll)
-        authorize("/csrf", permitAll)
-        authorize(anyRequest, authenticated)
-      }
-      oauth2ResourceServer {
-        jwt {
-          jwtAuthenticationConverter = AuthAwareTokenConverter()
-        }
-      }
-    }
+class ResourceServerConfiguration {
+  @Bean
+  fun configure(http: HttpSecurity): DefaultSecurityFilterChain? {
+    http.authorizeHttpRequests()
+      .requestMatchers("/health/**").permitAll()
+      .requestMatchers("/info").permitAll()
+      .requestMatchers("/prometheus").permitAll()
+      .requestMatchers("/v3/api-docs/**").permitAll()
+      .requestMatchers("/swagger-ui/**").permitAll()
+      .requestMatchers("/swagger-ui.html").permitAll()
+      .requestMatchers("/swagger-resources").permitAll()
+      .requestMatchers("/swagger-resources/configuration/ui").permitAll()
+      .requestMatchers("/swagger-resources/configuration/security").permitAll()
+      .requestMatchers("/webjars/**").permitAll()
+      .requestMatchers("/favicon.ico").permitAll()
+      .requestMatchers("/csrf").permitAll()
+      .anyRequest().authenticated()
+      .and()
+      .csrf()
+      .disable()
+      .oauth2ResourceServer().jwt().jwtAuthenticationConverter { AuthAwareTokenConverter().convert(it) }
+    return http.build()
   }
 }
