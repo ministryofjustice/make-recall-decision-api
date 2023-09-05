d2c43ad0e82054d0d54c07a61bb71c3c83f7b934
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2023-09-05 11:37:15 +0100

    MRD-1712 Use compatible flyway version

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,9 @@ dependencies {
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-  implementation("org.flywaydb:flyway-core:9.22.0")
+  // Going to flyway-core 9.20 needs SpringBoot version >3.1.0
+  // https://github.com/flyway/flyway/issues/3694
+  implementation("org.flywaydb:flyway-core:9.19.4")
   implementation("org.postgresql:postgresql:42.6.0")
 
   implementation("io.sentry:sentry-spring-boot-starter:6.28.0")
