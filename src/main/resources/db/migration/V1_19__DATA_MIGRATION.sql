1264e53601b0a3e1b589580049b02c8578d73683
Bill Sclater <bill.sclater@digital.justice.gov.uk>
2023-11-23 13:39:18 +0000

    mrd-2089_correction-to-ppcs-closed-status-data-migration: replaced randomly generated id with max+1 to avoid collision

--- src/main/resources/db/migration/V1_19__DATA_MIGRATION.sql
+++ src/main/resources/db/migration/V1_19__DATA_MIGRATION.sql
@@ -1,4 +1,4 @@
--- add a DNTR_DOWNLOADED status for existing (legacy) CLOSED, NO_RECALL_DECIDED statuses
+-- add a DNTR_DOWNLOADED status for existing (legacy) recommendations with both CLOSED and NO_RECALL_DECIDED statuses
 do
 $$
     declare
@@ -13,14 +13,14 @@ $$
                 INSERT INTO public.recommendation_status (id, recommendation_id, created_by, created_by_user_full_name,
                                                           created, modified_by, modified_by_user_full_name, modified,
                                                           name, active, recommendation_history_id, email_address)
-                VALUES ((SELECT random() * 999999999 + 1 AS RAND_1_11), f.recommendation_id, 'migration-script-ppcs',
+                VALUES ((select max(id) from public.recommendation_status) + 1), f.recommendation_id, 'migration-script-ppcs',
                         'migration-script-ppcs', now(), null, null, null, 'DNTR_DOWNLOADED',
                         true, null, null);
             end loop;
     end ;
 $$;
 
--- add a BOOK_TO_PPUD status for existing (legacy) CLOSED, RECALL_DECIDED statuses
+-- add a BOOK_TO_PPUD status for existing (legacy) recommendations with both CLOSED and RECALL_DECIDED statuses
 do
 $$
     declare
@@ -35,7 +35,7 @@ $$
                 INSERT INTO public.recommendation_status (id, recommendation_id, created_by, created_by_user_full_name,
                                                           created, modified_by, modified_by_user_full_name, modified,
                                                           name, active, recommendation_history_id, email_address)
-                VALUES ((SELECT random() * 999999999 + 1 AS RAND_1_11), f.recommendation_id, 'migration-script-ppcs',
+                VALUES ((select max(id) from public.recommendation_status) + 1), f.recommendation_id, 'migration-script-ppcs',
                         'migration-script-ppcs', now(), null, null, null, 'BOOK_TO_PPUD',
                         true, null, null);
             end loop;
