6d243cfb9d2c5693f46a918b598c6f1c1e15de46
Bill Sclater <bill.sclater@digital.justice.gov.uk>
2023-11-24 14:19:47 +0000

    mrd-2089_correction-to-ppcs-closed-status-data-migration:removed specifying id in query to avoid collision

--- src/main/resources/db/migration/V1_19__DATA_MIGRATION.sql
+++ src/main/resources/db/migration/V1_19__DATA_MIGRATION.sql
@@ -10,10 +10,10 @@ $$
              where name = 'NO_RECALL_DECIDED'
              group by recommendation_id)
             loop
-                INSERT INTO public.recommendation_status (id, recommendation_id, created_by, created_by_user_full_name,
+                INSERT INTO public.recommendation_status (recommendation_id, created_by, created_by_user_full_name,
                                                           created, modified_by, modified_by_user_full_name, modified,
                                                           name, active, recommendation_history_id, email_address)
-                VALUES ((select max(id) from public.recommendation_status) + 1), f.recommendation_id, 'migration-script-ppcs',
+                VALUES (f.recommendation_id, 'migration-script-ppcs',
                         'migration-script-ppcs', now(), null, null, null, 'DNTR_DOWNLOADED',
                         true, null, null);
             end loop;
@@ -32,10 +32,10 @@ $$
              where name = 'RECALL_DECIDED'
              group by recommendation_id)
             loop
-                INSERT INTO public.recommendation_status (id, recommendation_id, created_by, created_by_user_full_name,
+                INSERT INTO public.recommendation_status (recommendation_id, created_by, created_by_user_full_name,
                                                           created, modified_by, modified_by_user_full_name, modified,
                                                           name, active, recommendation_history_id, email_address)
-                VALUES ((select max(id) from public.recommendation_status) + 1), f.recommendation_id, 'migration-script-ppcs',
+                VALUES (f.recommendation_id, 'migration-script-ppcs',
                         'migration-script-ppcs', now(), null, null, null, 'BOOK_TO_PPUD',
                         true, null, null);
             end loop;
