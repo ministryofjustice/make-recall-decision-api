b226adb16202142e646f05969647270a9d8ffde5
Robin de Villiers <robindevilliers@me.com>
2024-01-09 12:42:53 +0000

    MRD-2238 - linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/recommendation/BookRecallToPpud.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/recommendation/BookRecallToPpud.kt
@@ -21,9 +21,9 @@ data class BookRecallToPpud(
   val gender: String? = null,
   val ethnicity: String? = null,
   val firstNames: String? = null,
-  //deprecated - keep for now to prevent dev from breaking.
+  // deprecated - keep for now to prevent dev from breaking.
   val firstName: String? = null,
-  //deprecated - keep for now to prevent dev from breaking.
+  // deprecated - keep for now to prevent dev from breaking.
   val secondName: String? = null,
   val lastName: String? = null,
   val dateOfBirth: LocalDate? = null,
