0c55e8fc98a0e376574c42416b608c815928cad1
Daniel Lackenby <daniel.lackenby@digital.justice.gov.uk>
2025-05-19 11:54:45 +0100

    Update PrisonApiService to collect consecutiveToSequence for Sentences and calculate the setences this group each with

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/Sentence.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/makerecalldecisions/Sentence.kt
@@ -7,6 +7,8 @@ data class Sentence(
   val bookingId: Int? = null,
   val sentenceSequence: Int? = null,
   val lineSequence: Int? = null,
+  val consecutiveToSequence: Int? = null,
+  val consecutiveGroup: List<Int> = listOf(),
   val caseSequence: Int? = null,
   val courtDescription: String? = null,
   val sentenceStatus: String? = null,
