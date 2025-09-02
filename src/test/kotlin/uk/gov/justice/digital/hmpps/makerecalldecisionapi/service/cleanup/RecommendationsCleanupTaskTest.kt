c2d2b9db03b204190834b2d2462f96c47f323694
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-09-02 12:14:27 +0100

    MRD-2856 Fix clean-up test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/cleanup/RecommendationsCleanupTaskTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/cleanup/RecommendationsCleanupTaskTest.kt
@@ -149,8 +149,8 @@ class RecommendationsCleanupTaskTest {
     lockAssertMock.use {
       val idsOfActiveRecommendationsNotYetDownloaded = List(43, { randomLong() })
       // TODO update the threshold values below based on config for your roll-out
-      val thresholdStartDate = ZonedDateTime.now().minusDays(cleanUpConfiguration.recurrent.lookBackInDays - 1)
-      val thresholdEndDate = ZonedDateTime.now()
+      val thresholdStartDate: ZonedDateTime = any()
+      val thresholdEndDate: ZonedDateTime = any()
       given(recommendationRepository.findActiveRecommendationsNotYetDownloaded(thresholdStartDate, thresholdEndDate))
         .willReturn(idsOfActiveRecommendationsNotYetDownloaded)
 
@@ -180,7 +180,7 @@ class RecommendationsCleanupTaskTest {
           .sendSystemDeleteRecommendationEvent(it.data.crn, it.data.createdBy!!)
       }
 
-      val startUpMessage = listOf("FTR48 clean-up task started")
+      val startUpMessage = listOf("<project/roll-out name> clean-up task started")
       val deletedRecommendationIdMessages = listOf(
         "The recommendations with the following IDs were soft deleted, as they were" +
           " active but not yet downloaded: ${idsOfActiveRecommendationsNotYetDownloaded.subList(0, 20)}",
@@ -192,7 +192,7 @@ class RecommendationsCleanupTaskTest {
       val deletionDomainEventMessages = activeRecommendationsNotYetDownloaded.map {
         "System delete domain event sent for crn::'${it.data.crn}' username::'${it.data.createdBy}"
       }
-      val endMessage = listOf("FTR48 clean-up task ended")
+      val endMessage = listOf("<project/roll-out name> clean-up task ended")
       val expectedInfoMessages =
         startUpMessage + deletedRecommendationIdMessages + deletionDomainEventMessages + endMessage
       with(logAppender.list) {
@@ -222,8 +222,8 @@ class RecommendationsCleanupTaskTest {
     lockAssertMock.use {
       val idsOfActiveRecommendationsNotYetDownloaded = List(43, { randomLong() })
       // TODO update the threshold values below based on config for your roll-out
-      val thresholdStartDate = ZonedDateTime.now().minusDays(cleanUpConfiguration.recurrent.lookBackInDays - 1)
-      val thresholdEndDate = ZonedDateTime.now()
+      val thresholdStartDate: ZonedDateTime = any()
+      val thresholdEndDate: ZonedDateTime = any()
       given(recommendationRepository.findActiveRecommendationsNotYetDownloaded(thresholdStartDate, thresholdEndDate))
         .willReturn(idsOfActiveRecommendationsNotYetDownloaded)
 
@@ -235,7 +235,7 @@ class RecommendationsCleanupTaskTest {
       inOrder.verify(it, LockAssert::assertLocked)
       then(recommendationRepository).should(inOrder).softDeleteByIds(idsOfActiveRecommendationsNotYetDownloaded)
 
-      val startUpMessage = listOf("FTR48 clean-up task started")
+      val startUpMessage = listOf("<project/roll-out name> clean-up task started")
       val deletedRecommendationIdMessages = listOf(
         "The recommendations with the following IDs were soft deleted, as they were" +
           " active but not yet downloaded: ${idsOfActiveRecommendationsNotYetDownloaded.subList(0, 20)}",
@@ -244,7 +244,7 @@ class RecommendationsCleanupTaskTest {
         "The recommendations with the following IDs were soft deleted, as they were" +
           " active but not yet downloaded: ${idsOfActiveRecommendationsNotYetDownloaded.subList(40, 43)}",
       )
-      val endMessage = listOf("FTR48 clean-up task ended")
+      val endMessage = listOf("<project/roll-out name> clean-up task ended")
       val expectedInfoMessages = startUpMessage + deletedRecommendationIdMessages + endMessage
       with(logAppender.list) {
         this.forEach { assertThat(it.level).isEqualTo(Level.INFO) }
