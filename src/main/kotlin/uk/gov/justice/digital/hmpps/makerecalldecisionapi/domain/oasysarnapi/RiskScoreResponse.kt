72c3db46fd1285945e52b545b3d5d5c5ea7e7713
Suresh Mandalapu <231466518+sureshmoj@users.noreply.github.com>
2026-01-08 09:11:10 +0000

    make fields nullable

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/oasysarnapi/RiskScoreResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/oasysarnapi/RiskScoreResponse.kt
@@ -20,103 +20,103 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
   JsonSubTypes.Type(value = AssessmentScoresV2::class, name = "2"),
 )
 sealed class AssessmentScores {
-  abstract val completedDate: String
-  abstract val status: AssessmentStatus
-  abstract val outputVersion: String
+  abstract val completedDate: String?
+  abstract val status: AssessmentStatus?
+  abstract val outputVersion: String?
 }
 
 data class AssessmentScoresV1(
-  override val completedDate: String,
-  override val status: AssessmentStatus,
-  override val outputVersion: String,
-  val output: OutputV1,
+  override val completedDate: String?,
+  override val status: AssessmentStatus?,
+  override val outputVersion: String?,
+  val output: OutputV1?,
 ) : AssessmentScores()
 
 data class AssessmentScoresV2(
-  override val completedDate: String,
-  override val status: AssessmentStatus,
-  override val outputVersion: String,
-  val output: OutputV2,
+  override val completedDate: String?,
+  override val status: AssessmentStatus?,
+  override val outputVersion: String?,
+  val output: OutputV2?,
 ) : AssessmentScores()
 
 data class OutputV1(
-  val groupReconvictionScore: GroupReconvictionScore,
-  val violencePredictorScore: ViolencePredictorScore,
-  val generalPredictorScore: GeneralPredictorScore,
-  val riskOfSeriousRecidivismScore: RiskOfSeriousRecidivismScore,
-  val sexualPredictorScore: SexualPredictorScore,
+  val groupReconvictionScore: GroupReconvictionScore?,
+  val violencePredictorScore: ViolencePredictorScore?,
+  val generalPredictorScore: GeneralPredictorScore?,
+  val riskOfSeriousRecidivismScore: RiskOfSeriousRecidivismScore?,
+  val sexualPredictorScore: SexualPredictorScore?,
 )
 
 data class GroupReconvictionScore(
-  val oneYear: Int,
-  val twoYears: Int,
-  val scoreLevel: FourLevelRiskScoreLevel,
+  val oneYear: Int?,
+  val twoYears: Int?,
+  val scoreLevel: FourLevelRiskScoreLevel?,
 )
 
 data class ViolencePredictorScore(
-  val ovpStaticWeightedScore: Int,
-  val ovpDynamicWeightedScore: Int,
-  val ovpTotalWeightedScore: Int,
-  val oneYear: Int,
-  val twoYears: Int,
-  val ovpRisk: FourLevelRiskScoreLevel,
+  val ovpStaticWeightedScore: Int?,
+  val ovpDynamicWeightedScore: Int?,
+  val ovpTotalWeightedScore: Int?,
+  val oneYear: Int?,
+  val twoYears: Int?,
+  val ovpRisk: FourLevelRiskScoreLevel?,
 )
 
 data class GeneralPredictorScore(
-  val ogpStaticWeightedScore: Int,
-  val ogpDynamicWeightedScore: Int,
-  val ogpTotalWeightedScore: Int,
-  val ogp1Year: Int,
-  val ogp2Year: Int,
-  val ogpRisk: FourLevelRiskScoreLevel,
+  val ogpStaticWeightedScore: Int?,
+  val ogpDynamicWeightedScore: Int?,
+  val ogpTotalWeightedScore: Int?,
+  val ogp1Year: Int?,
+  val ogp2Year: Int?,
+  val ogpRisk: FourLevelRiskScoreLevel?,
 )
 
 data class RiskOfSeriousRecidivismScore(
-  val percentageScore: Int,
+  val percentageScore: Int?,
   // The 3 fields commented out below weren't there before these changes. Probably
   // OK to leave them out, since we don't seem to be using them anywhere (should we?)
 //  val staticOrDynamic: StaticOrDynamic,
 //  val source: String,
 //  val algorithmVersion: String,
-  val scoreLevel: ThreeLevelRiskScoreLevel,
+  val scoreLevel: ThreeLevelRiskScoreLevel?,
 )
 
 data class SexualPredictorScore(
-  val ospIndecentPercentageScore: Int,
-  val ospContactPercentageScore: Int,
-  val ospIndecentScoreLevel: ThreeLevelRiskScoreLevel,
-  val ospContactScoreLevel: FourLevelRiskScoreLevel,
-  val ospIndirectImagePercentageScore: Int,
-  val ospDirectContactPercentageScore: Int,
-  val ospIndirectImageScoreLevel: ThreeLevelRiskScoreLevel,
-  val ospDirectContactScoreLevel: FourLevelRiskScoreLevel,
+  val ospIndecentPercentageScore: Int?,
+  val ospContactPercentageScore: Int?,
+  val ospIndecentScoreLevel: ThreeLevelRiskScoreLevel?,
+  val ospContactScoreLevel: FourLevelRiskScoreLevel?,
+  val ospIndirectImagePercentageScore: Int?,
+  val ospDirectContactPercentageScore: Int?,
+  val ospIndirectImageScoreLevel: ThreeLevelRiskScoreLevel?,
+  val ospDirectContactScoreLevel: FourLevelRiskScoreLevel?,
 )
 
 data class OutputV2(
-  val allReoffendingPredictor: StaticOrDynamicPredictor,
-  val violentReoffendingPredictor: StaticOrDynamicPredictor,
-  val seriousViolentReoffendingPredictor: StaticOrDynamicPredictor,
-  val directContactSexualReoffendingPredictor: Predictor,
-  val indirectImageContactSexualReoffendingPredictor: Predictor,
-  val combinedSeriousReoffendingPredictor: CombinedPredictor,
+  val allReoffendingPredictor: StaticOrDynamicPredictor?,
+  val violentReoffendingPredictor: StaticOrDynamicPredictor?,
+  val seriousViolentReoffendingPredictor: StaticOrDynamicPredictor?,
+  val directContactSexualReoffendingPredictor: Predictor?,
+  val indirectImageContactSexualReoffendingPredictor: Predictor?,
+  val combinedSeriousReoffendingPredictor: CombinedPredictor?,
 )
 
 data class Predictor(
-  val score: Int,
-  val band: FourBandRiskScoreBand,
+  val score: Int?,
+  val band: FourBandRiskScoreBand?,
 )
 
 data class StaticOrDynamicPredictor(
-  val score: Int,
-  val band: FourBandRiskScoreBand,
-  val staticOrDynamic: StaticOrDynamic,
+  val score: Int?,
+  val band: FourBandRiskScoreBand?,
+  val staticOrDynamic: StaticOrDynamic?,
 )
 
 data class CombinedPredictor(
-  val score: Int,
-  val band: FourBandRiskScoreBand,
-  val staticOrDynamic: StaticOrDynamic,
-  val algorithmVersion: String,
+  val score: Int?,
+  val band: FourBandRiskScoreBand?,
+  val staticOrDynamic: StaticOrDynamic?,
+  val algorithmVersion: String?,
 )
 
 enum class StaticOrDynamic {
