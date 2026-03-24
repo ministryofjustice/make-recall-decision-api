dfb04ddc61dd5efeecf80cf3983add0971cfec11
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2026-03-24 17:25:16 +0000

    MRD-3033 Fix JSON

--- src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/integration/requests/makerecalldecisions/UpdateRecommendationRequestWithInvalidRecallType.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/integration/requests/makerecalldecisions/UpdateRecommendationRequestWithInvalidRecallType.kt
@@ -221,8 +221,8 @@ fun updateRecommendationRequestWithInvalidRecallType(status: Status = Status.DRA
         "details": "Behaviour leading to sexual or violent behaviour"
       },
       {
-        "value": "BEHAVIOUR_LIKELY_TO_RESULT_SEXUAL_OR_VIOLENT_OFFENCE"
-        "details": "Behaviour likely to result in sexual or violent behaviour",
+        "value": "BEHAVIOUR_LIKELY_TO_RESULT_SEXUAL_OR_VIOLENT_OFFENCE",
+        "details": "Behaviour likely to result in sexual or violent behaviour"
       },
       {
         "value": "OUT_OF_TOUCH",
