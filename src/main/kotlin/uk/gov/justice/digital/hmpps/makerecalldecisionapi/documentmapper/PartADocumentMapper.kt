22ac54d2af4dc6d88ff43800964cf3ae1a148424
fincardy-moj <fin.cardy@justice.gov.uk>
2026-04-10 15:56:52 +0100

    MRD-3172 update so criteria shows/hides correctly

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/documentmapper/PartADocumentMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/documentmapper/PartADocumentMapper.kt
@@ -158,10 +158,7 @@ internal class PartADocumentMapper(
         recommendation.isYouthSentenceOver12Months,
         recommendation,
       ),
-      isYouthChargedWithSeriousOffence = generateExclusionCriteriaAnswer(
-        recommendation.isYouthChargedWithSeriousOffence,
-        recommendation,
-      ),
+      isYouthChargedWithSeriousOffence = calculateIsYouthChargedWithSeriousOffence(recommendation),
       isExtendedSentence = convertBooleanToYesNo(recommendation.calculateIsExtendedSentence()),
       hasVictimsInContactScheme = recommendation.hasVictimsInContactScheme?.selected?.partADisplayValue
         ?: EMPTY_STRING,
@@ -574,6 +571,18 @@ internal class PartADocumentMapper(
     convertBooleanToYesNo(recommendation.isMappaCategory4)
   } else if (recommendation.sentenceGroup === SentenceGroup.INDETERMINATE || recommendation.sentenceGroup === SentenceGroup.EXTENDED) {
     generateExclusionCriteriaAnswer(false, recommendation)
+  } else {
+    null
+  }
+
+  /**
+   * Similar logic to the other calculate... functions, for this field we don't want to display any value unless the
+   * offender is serving a youth sentence, and that sentence is over 12 months
+   */
+  private fun calculateIsYouthChargedWithSeriousOffence(recommendation: RecommendationResponse): String? = if (recommendation.sentenceGroup == SentenceGroup.YOUTH_SDS &&
+    !(recommendation.isYouthSentenceOver12Months ?: true)
+  ) {
+    convertBooleanToYesNo(recommendation.isYouthChargedWithSeriousOffence)
   } else {
     null
   }
