91ca739a8b265332c8015060fa3db56c5ec554f0
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2026-07-30 15:08:51 +0100

    MRD-3023 Support FTR56 Offence Conviction template

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/config/documenttemplate/DocumentTemplateVersion.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/config/documenttemplate/DocumentTemplateVersion.kt
@@ -9,10 +9,11 @@ enum class PartATemplateVersion(val flagVariantKey: String, override val path: S
   FTR48("ftr48", "2025-09-02 - FTR48 Phase 1"),
   RISK_TO_SELF("riskToSelf", "2026-02-02 - Risk to self"),
   FTR56("ftr56", "2026-03-31 - FTR56"),
+  FTR56_OFFENCE_CONVICTION("ftr56OffenceConviction", "2026-07-29 - FTR56 Offence conviction"),
   ;
 
   companion object {
-    fun forVariantKey(flagVariantKey: String): PartATemplateVersion = PartATemplateVersion.values().first { it.flagVariantKey == flagVariantKey }
+    fun forVariantKey(flagVariantKey: String): PartATemplateVersion = entries.first { it.flagVariantKey == flagVariantKey }
   }
 }
 
