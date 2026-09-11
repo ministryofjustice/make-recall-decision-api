7f08ab7f82a91df4eda02f00f5019453b910474a
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2026-09-11 18:27:44 +0100

    MRD-2575 Add PPUD User Mapping CRUD operations
    Added to support PPCS admin pages to configure the CaR-to-PPUD user mappings.
    Tests pending.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/controller/PpudUserMappingController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/controller/PpudUserMappingController.kt
@@ -7,13 +7,18 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.PpudUserMapping
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.PpudUserMappingResponse
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.PpudUserMappingSearchRequest
+import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.entity.PpudUserMappingEntity
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.service.PpudUserMappingService
 
 @RestController
@@ -35,5 +40,53 @@ internal class PpudUserMappingController(
     val response = ppudUserMappingService.findByUserNameIgnoreCase(request.userName)
     val ppudMapping = response?.let { PpudUserMapping(it) }
     return ResponseEntity(PpudUserMappingResponse(ppudMapping), HttpStatus.OK)
+  }
+
+  @PreAuthorize("hasRole('ROLE_MAKE_RECALL_DECISION')")
+  @GetMapping("/user-mapping")
+  @Operation(summary = "Gets all PPUD user mappings.")
+  suspend fun getAllUserMappings(): ResponseEntity<List<PpudUserMappingEntity>> {
+    log.info(normalizeSpace("Get all PPUD user mappings endpoint hit"))
+    return ResponseEntity(ppudUserMappingService.getAllUserMappings(), HttpStatus.OK)
+  }
+
+  @PreAuthorize("hasRole('ROLE_MAKE_RECALL_DECISION')")
+  @GetMapping("/user-mapping/{id}")
+  @Operation(summary = "Gets a PPUD user mapping by ID.")
+  suspend fun getUserMappingById(@PathVariable(required = true) id: Long): ResponseEntity<PpudUserMappingEntity> {
+    log.info(normalizeSpace("Get PPUD user mapping endpoint hit for id: $id"))
+    val response = ppudUserMappingService.findById(id)
+    return if (response != null) {
+      ResponseEntity(response, HttpStatus.OK)
+    } else {
+      ResponseEntity(HttpStatus.NOT_FOUND)
+    }
+  }
+
+  @PreAuthorize("hasRole('ROLE_MAKE_RECALL_DECISION')")
+  @PostMapping("/user-mapping")
+  @Operation(summary = "Creates a PPUD user mapping.")
+  suspend fun createUserMapping(
+    @RequestBody(required = true) ppudUserMappingEntity: PpudUserMappingEntity,
+  ): ResponseEntity<PpudUserMappingEntity> =
+    ResponseEntity(ppudUserMappingService.saveUserMapping(ppudUserMappingEntity), HttpStatus.CREATED)
+
+  @PreAuthorize("hasRole('ROLE_MAKE_RECALL_DECISION')")
+  @PutMapping("/user-mapping/{id}")
+  @Operation(summary = "Updates a PPUD user mapping.")
+  suspend fun updateUserMapping(
+    @PathVariable(required = true) id: String,
+    @RequestBody(required = true) ppudUserMappingEntity: PpudUserMappingEntity,
+  ): ResponseEntity<PpudUserMappingEntity> =
+    ResponseEntity(ppudUserMappingService.updateUserMapping(id.toLong(), ppudUserMappingEntity), HttpStatus.OK)
+
+  @PreAuthorize("hasRole('ROLE_MAKE_RECALL_DECISION')")
+  @DeleteMapping("/user-mapping/{id}")
+  @Operation(summary = "Deletes a PPUD user mapping.")
+  suspend fun deleteUserMapping(
+    @PathVariable(required = true) id: String,
+  ): ResponseEntity<Unit> {
+    ppudUserMappingService.deleteUserMapping(id.toLong())
+    return ResponseEntity(HttpStatus.NO_CONTENT)
   }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/PpudUserMappingService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/PpudUserMappingService.kt
@@ -13,4 +13,30 @@ internal class PpudUserMappingService(
   fun findByUserNameIgnoreCase(
     username: String,
   ): PpudUserMappingEntity? = ppudUserMappingRepository.findByUserNameIgnoreCase(username)
+
+  fun findById(
+    id: Long,
+  ) = ppudUserMappingRepository.findById(id).orElse(null)
+
+  fun getAllUserMappings() = ppudUserMappingRepository.findAll()
+
+  fun saveUserMapping(
+    ppudUserMappingEntity: PpudUserMappingEntity,
+  ): PpudUserMappingEntity = ppudUserMappingRepository.save(ppudUserMappingEntity)
+
+  fun deleteUserMapping(
+    id: Long,
+  ) = ppudUserMappingRepository.deleteById(id)
+
+  fun updateUserMapping(
+    id: Long,
+    ppudUserMappingEntity: PpudUserMappingEntity,
+  ): PpudUserMappingEntity {
+    val existingMapping = ppudUserMappingRepository.findById(id).orElseThrow { Exception("User mapping not found") }
+    existingMapping.userName = ppudUserMappingEntity.userName
+    existingMapping.ppudUserFullName = ppudUserMappingEntity.ppudUserFullName
+    existingMapping.ppudTeamName = ppudUserMappingEntity.ppudTeamName
+    existingMapping.ppudUserName = ppudUserMappingEntity.ppudUserName
+    return ppudUserMappingRepository.save(existingMapping)
+  }
 }
