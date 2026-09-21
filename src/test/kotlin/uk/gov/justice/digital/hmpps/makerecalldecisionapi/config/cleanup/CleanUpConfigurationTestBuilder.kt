c1ff39f2734ed1b222cdd88b9c949d4f7a6f7e6e
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2026-09-21 15:30:02 +0100

    MRD-3380 Fix test builder function name

--- src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/config/cleanup/CleanUpConfigurationTestBuilder.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/config/cleanup/CleanUpConfigurationTestBuilder.kt
@@ -7,14 +7,14 @@ import java.time.ZonedDateTime
 
 internal fun cleanUpConfiguration(
   recurrent: RecurrentCleanUpConfiguration = recurrentCleanUpConfiguration(),
-  ftr56: NewStandardLicenceConditionsCleanUpConfiguration = ftr56CleanUpConfiguration(),
-) = CleanUpConfiguration(recurrent, ftr56)
+  newStandardLicenceConditions: NewStandardLicenceConditionsCleanUpConfiguration = newStandardLicenceConditions(),
+) = CleanUpConfiguration(recurrent, newStandardLicenceConditions)
 
 internal fun recurrentCleanUpConfiguration(
   lookBackInDays: Long = randomLong(),
 ) = RecurrentCleanUpConfiguration(lookBackInDays)
 
-internal fun ftr56CleanUpConfiguration(
+internal fun newStandardLicenceConditions(
   thresholdDateTime: ZonedDateTime = randomZonedDateTime(),
   cron: String = randomString(),
 ) = NewStandardLicenceConditionsCleanUpConfiguration(thresholdDateTime, cron)
